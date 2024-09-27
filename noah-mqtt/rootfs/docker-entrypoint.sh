41fa174d388900e9c6f147cda4f1f27248af828e
Markus Troßbach <markus@trossbach.net>
2024-09-27 07:56:47 +0200

    add support for custom mqtt settings

--- noah-mqtt/rootfs/docker-entrypoint.sh
+++ noah-mqtt/rootfs/docker-entrypoint.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bashio
 export VERSION="$(jq --raw-output '.version // empty' config.json)"
-export MQTT_HOST="$(bashio::services 'mqtt' 'host')"
-export MQTT_PORT="$(bashio::services 'mqtt' 'port')"
-export MQTT_USERNAME="$(bashio::services 'mqtt' 'username')"
-export MQTT_PASSWORD="$(bashio::services 'mqtt' 'password')"
+
+if (bashio::config.is_empty 'mqtt' || ! (bashio::config.has_value 'mqtt.server' || bashio::config.has_value 'mqtt.user' || bashio::config.has_value 'mqtt.password')) && bashio::var.has_value "$(bashio::services 'mqtt')"; then
+    export MQTT_HOST="$(bashio::services 'mqtt' 'host')"
+    export MQTT_PORT="$(bashio::services 'mqtt' 'port')"
+    export MQTT_USERNAME="$(bashio::services 'mqtt' 'username')"
+    export MQTT_PASSWORD="$(bashio::services 'mqtt' 'password')"
+else
+    export MQTT_HOST="$(bashio::config 'mqtt.host')"
+    export MQTT_PORT="$(bashio::config 'mqtt.port')"
+    export MQTT_USERNAME="$(bashio::config 'mqtt.username')"
+    export MQTT_PASSWORD="$(bashio::config 'mqtt.password')"
+fi
+
 export GROWATT_USERNAME="$(bashio::config 'growatt_username')"
 export GROWATT_PASSWORD="$(bashio::config 'growatt_password')"
 export LOG_LEVEL="$(bashio::config 'log_level')"
